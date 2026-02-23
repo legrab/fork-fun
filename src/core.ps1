58d83084ca8dbd59c5dc171fb4d62b1d4279ae19
legrab <b.geller@advanced-engineering.at>
2026-02-23 14:00:53 +0100

    refact(core): extract Get-Pizza
    # Conflicts:
    #	src/main.ps1

--- src/core.ps1
+++ src/core.ps1
@@ -1,6 +1,10 @@
 # Select a pizza to order
 
-$pizza = @{ Name = "some" }
+function Get-Pizza() {
+    return @{ Name = "some" }
+}
+
+$pizza = Get-Pizza;
 
 . ./src/order.ps1
 Submit-Order $pizza.Name

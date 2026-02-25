33b114327f62b1a883d86b665ae47145a585153b
Hiasinator <m.krallinger@advanced-engineering.at>
2026-02-25 14:25:17 +0100

    Revert "chore(main): order my pizza"
    This reverts commit 08934511cdd408988a31031b30795fd6e3872723.

--- src/main.ps1
+++ src/main.ps1
@@ -1,8 +1,10 @@
+Write-Output "Ordering Primavera pizza"
 function Get-Pizza() {
     return @{ Name = "some" }
 }
 
 $pizza = Get-Pizza;
+$pizza = @{ Name = "some" }
 
 . ./src/order.ps1
-Submit-Order $pizza.Name
+Submit-Order $pizzaName
